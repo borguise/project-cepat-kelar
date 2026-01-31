d4ea9e603d0951f1dafe4fa3072a2b99d15eca26
fadhilaldien <famudial@gmail.com>
2026-01-31 18:16:15 +0800

    initial commit

---
+++ project-cepat-kelar-jpa/src/main/java/com/project/cepat/kelar/jpa/ProjectCepatKelarJpaApplication.java
@@ -0,0 +1,13 @@
+package com.project.cepat.kelar.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCepatKelarJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCepatKelarJpaApplication.class, args);
+	}
+
+}
---
+++ project-cepat-kelar-jpa/src/test/java/com/project/cepat/kelar/jpa/ProjectCepatKelarJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cepat.kelar.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCepatKelarJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
